26da623688e18ad11edc222159e2c93e7de538eb
Bottz-3 <william_botterill@outlook.com>
2026-03-24 11:50:42 +1030

    Forgor to commit

--- src/cpu/alu/bitwise.rs
+++ src/cpu/alu/bitwise.rs
@@ -59,3 +59,31 @@ impl Cpu {
         self.registers.set(Register::F, f);
     }
 }
+// Test, reset and set bit
+impl Cpu {
+    pub fn test_bit(&mut self, bit: u8, reg: Register) {
+        let reg_val = self.registers.get(reg);
+        let mut f = self.registers.get(Register::F);
+
+        if (reg_val >> bit) & 1 == 0 {
+            f |= 1 << 7;
+        } else {
+            f &= !(1 << 7);
+        }
+        f |= 1 << 5;
+        f &= !(1 << 6);
+        self.registers.set(Register::F, f);
+    }
+    pub fn reset_bit(&mut self, bit: u8, reg: Register) {
+        let reg_val = self.registers.get(reg);
+
+        let res = reg_val & !(1 << bit);
+
+        self.registers.set(reg, res);
+    }
+    pub fn set_bit(&mut self, bit: u8, reg: Register) {
+        let reg_val = self.registers.get(reg);
+        let res = reg_val | (1 << bit);
+        self.registers.set(reg, res);
+    }
+}
