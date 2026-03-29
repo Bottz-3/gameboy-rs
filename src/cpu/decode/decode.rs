78104d8b037d0f0d7c7812b951f6f309bff1399d
Bottz-3 <william_botterill@outlook.com>
2026-03-29 21:51:35 +1030

    Get rid of unreachable in match statement

--- src/cpu/decode/decode.rs
+++ src/cpu/decode/decode.rs
@@ -7,7 +7,6 @@ impl Cpu {
             0x40..=0x7F => self.decode_loads(opcode),
             0x80..=0xBF => self.decode_alu(opcode),
             0xC0..=0xFF => self.decode_control_flow(opcode),
-            _ => panic!("INVALID OPCODE!"),
         }
     }
 }
