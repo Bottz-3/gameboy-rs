9063bc80d4d14b3f98fdcdbdeb3700eaf3e1a05a
Bottz-3 <william_botterill@outlook.com>
2026-04-30 19:42:03 +0930

    Give gb_loop the cycles

--- src/gb_loop.rs
+++ src/gb_loop.rs
@@ -4,11 +4,11 @@ use crate::mmu::mmu::Mmu;
 impl Cpu {
     pub fn gb_loop(&mut self) {
         self.handle_interrupts();
+        let mut cycles = 4;
         if !self.halted {
             let opcode = self.fetch_u8();
-            self.decode(opcode);
+            cycles = self.decode(opcode);
         }
-        let cycles = 4;
         if self.mmu.ppu.step(cycles) {
             let if_ = self.mmu.read(0xFF0F);
             self.mmu.write(0xFF0F, if_ | 0x01);
